b2d8bae68325e0d1c67077be654008237bf7fe0a
jennifer-ross <pguczol23@yandex.ru>
2023-09-07 21:41:13 +0500

    Maid App configuration && plugins

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import configuration from './config/configuration'
+import {
+	FastifyAdapter,
+	NestFastifyApplication,
+} from '@nestjs/platform-fastify'
+import compression from '@fastify/compress'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
-	await app.listen(3000)
+	const app = await NestFactory.create<NestFastifyApplication>(
+		AppModule,
+		new FastifyAdapter({
+			logger: configuration().logs,
+		}),
+		{
+			rawBody: true,
+		},
+	)
+
+	await app.setGlobalPrefix('api')
+	await app.register(compression, {
+		encodings: configuration().compressionEncodings,
+	})
+	await app.enableCors({
+		origin: '*',
+		methods: 'GET,HEAD,PUT,PATCH,POST',
+		credentials: true,
+	})
+
+	await app.listen(configuration().port, configuration().host)
 }
+
 bootstrap()

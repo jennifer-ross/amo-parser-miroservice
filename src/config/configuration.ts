14e25ed85aac7fe97cac48440cad594dcce22d13
jennifer-ross <pguczol23@yandex.ru>
2023-09-08 17:28:48 +0500

    Update configuration file

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,13 +4,15 @@ import * as process from 'process'
 import * as fs from 'fs'
 import * as path from 'path'
 import { Algorithm } from 'jsonwebtoken'
+import { VanillaPuppeteer } from 'puppeteer-extra'
 
 export default () => ({
 	isDev: process.env.NODE_ENV !== 'production' || 'production',
 	port: parseInt(process.env.PORT, 10) || 3000,
 	logs:
-		isBoolean(toLower(process.env.LOGS)) &&
-		toLower(process.env.LOGS) === 'true',
+		(isBoolean(toLower(process.env.LOGS)) &&
+			toLower(process.env.LOGS) === 'true') ||
+		true,
 	host: process.env.HOST || '127.0.0.1',
 	compressionEncodings: ['gzip', 'deflate'] as EncodingToken[],
 	saltRounds: parseInt(process.env.SALT_ROUNDS, 10) || 10,
@@ -49,4 +51,31 @@ export default () => ({
 	},
 	throttleTtl: parseInt(process.env.THROTTLE_TTL, 10) || 60,
 	throttleLimit: parseInt(process.env.THROTTLE_LIMIT, 10) || 10,
+
+	captcha: {
+		id: process.env.CAPTCHA_ID || '2captcha',
+		token: process.env.CAPTCHA_TOKEN || 'ENTER_YOUR_2CAPTCHA_API_KEY_HERE',
+	},
+	amo: {
+		saveSession:
+			(isBoolean(toLower(process.env.AMO_SAVE_SESSION)) &&
+				toLower(process.env.AMO_SAVE_SESSION) === 'true') ||
+			true,
+		login: process.env.AMO_LOGIN || 'pguczol23@yandex.ru',
+		password: process.env.AMO_PASSWORD || 'AStQs2lV',
+		crm: process.env.CRM_NAME || 'pguczol23',
+	},
+	parser: {
+		launch: {
+			headless: false,
+			ignoreHTTPSErrors: true,
+			args: [
+				`--window-size=800,600`,
+				'--disable-features=IsolateOrigins,site-per-process,SitePerProcess',
+				'--flag-switches-begin --disable-site-isolation-trials --flag-switches-end',
+			],
+		} as Parameters<VanillaPuppeteer['launch']>[0],
+		protocol: 'https',
+		domain: 'amocrm.ru',
+	},
 })

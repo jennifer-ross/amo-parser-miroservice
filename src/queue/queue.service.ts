58ed0ef60a0e1f6c4ff6671da8af9a7191407e82
jennifer23 <38167698+jennifer2318@users.noreply.github.com>
2023-09-23 13:08:12 +0500

    Added taskId to response && Subject for email message

--- src/queue/queue.service.ts
+++ src/queue/queue.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 import Queue from 'queue'
 
 @Injectable()
 export class QueueService {
 	private readonly queue = new Queue({ autostart: true })
 
-	async createJob(job: Function) {
-		const taskId = this.queue.push((cb) => {
+	async createJob(job: Function, taskId: string) {
+		this.queue.push((cb) => {
 			cb(null, job())
 		})
 
